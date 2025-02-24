0e6a84f21fd5cd6be8b4f1f989620da93e0d1369
Andre Eggenschwiler <swissi1984@gmail.com>
2025-02-24 16:56:52 +0100

    Monday

--- app/src/main/java/de/syntax_institut/jetpack/ClimateComparer/ui/theme/Color.kt
+++ app/src/main/java/de/syntax_institut/jetpack/ClimateComparer/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package de.syntax_institut.jetpack.ClimateComparer.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFF172FA8)
-val PurpleGrey80 = Color(0xFF1C30AD)
+val PurpleGrey80 = Color(0xFFD4D6DE)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF1F35A4)
