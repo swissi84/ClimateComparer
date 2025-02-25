ae99fc003301d67babef9a1ad4733530d3d0eb7b
Mario Lösel <loesel_mario@mail.com>
2025-02-25 11:41:31 +0100

    Einrichtung Room: FavoriteLocationsDao.kt # FavoriteLocationsDatabase.kt

--- app/src/main/java/de/syntax_institut/jetpack/ClimateComparer/data/local/FavoriteLocationsDao.kt
+++ app/src/main/java/de/syntax_institut/jetpack/ClimateComparer/data/local/FavoriteLocationsDao.kt
@@ -1,2 +1,21 @@
 package de.syntax_institut.jetpack.ClimateComparer.data.local
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteLocationsDao {
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertFavoriteLocation(favoriteLocation: FavoriteLocation)
+
+    @Query("SELECT * FROM favorite_locations")
+    fun getAllFavoriteLocations(): Flow<List<FavoriteLocation>>
+
+    @Delete
+    suspend fun deleteFavoriteLocation(favoriteLocation: FavoriteLocation)
+}
